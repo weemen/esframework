4f037b2556e43102fae386e4b4f83400ac5d0171
Leon Weemen <weemen@leonweemen.nl>
2018-10-14 10:18:35 +0200

    Applied PEP-254 after applying PEP-484 - suggested by scrutenizer

--- esframework/repository/__init__.py
+++ esframework/repository/__init__.py
@@ -2,6 +2,9 @@ import abc
 
 
 class Repository(object, metaclass=abc.ABCMeta):
+    """
+    Base repository class which should be extended for loading and saving
+    from different sources """
 
     __store = None
 
@@ -10,14 +13,17 @@ class Repository(object, metaclass=abc.ABCMeta):
 
     @abc.abstractmethod
     def load(self, aggregate_root):
+        """ Should be implemented by child class for loading from storage """
         raise NotImplementedError('Every repository must have an load method.')
 
     @abc.abstractmethod
     def save(self, aggregate_root):
+        """ Should be implemented by child class for saving to storage """
         raise NotImplementedError('Every repository must have an save method.')
 
 
 class InMemoryRepository(Repository):
+    """ An in Memory repository """
 
     def load(self, aggregate_root):
         pass

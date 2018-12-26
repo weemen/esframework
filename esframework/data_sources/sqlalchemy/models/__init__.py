1024d0bdab173b2f57fee7a83252526846677911
Leon Weemen <weemen@leonweemen.nl>
2018-12-26 12:51:38 +0100

    Lengths are required for MySQL

--- esframework/data_sources/sqlalchemy/models/__init__.py
+++ esframework/data_sources/sqlalchemy/models/__init__.py
@@ -7,15 +7,15 @@ Base = declarative_base()
 class SqlDomainRecord(Base):
     __tablename__ = 'event_store'
 
-    domain_event_id = Column(String, primary_key=True, nullable=False)
-    aggregate_root_id = Column(String, nullable=False)
+    domain_event_id = Column(String(length=36), primary_key=True, nullable=False)
+    aggregate_root_id = Column(String(length=36), nullable=False)
     version = Column(Integer, nullable=False)
-    domain_event_name = Column(String, nullable=False)
+    domain_event_name = Column(String(length=255), nullable=False)
     domain_event_body = Column(JSON, nullable=False)
-    store_date = Column(String, nullable=False)
-    event_date = Column(String, nullable=False)
-    correlation_id = Column(String, nullable=False)
-    causation_id = Column(String, nullable=False)
+    store_date = Column(String(length=50), nullable=False)
+    event_date = Column(String(length=50), nullable=False)
+    correlation_id = Column(String(length=36), nullable=False)
+    causation_id = Column(String(length=36), nullable=False)
     event_metadata = Column(JSON, nullable=False)
 
     UniqueConstraint('aggregate_root_id', 'version', name='uix_1')
